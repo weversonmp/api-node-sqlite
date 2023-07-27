ce42702db5e937447eecd475915da98ffa6b90c8
weversonmp <weversonmnzs@gmail.com>
2023-07-27 18:02:00 -0300

    Add sqlite db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
-const express = require('express');
+import { openDb } from './configDB.js';
+import express from 'express';
+
 const app = express();
+app.use(express.json());
+
+openDb();
 
 app.get('/', (req, res) => {
-  console.log('Hello World!!!');
+  res.send('Hello World!!!');
+});
+
+app.post('/pessoa', (req, res) => {
+  console.log(req.body)
+  res.json({
+    "statusCode": 200
+  })
 });
 
 app.listen(3000, () => console.log('Api rodando em http://localhost:3000'))
