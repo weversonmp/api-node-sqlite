ec20674566f6dbab00a48b23ec54dfb5dbaf43a7
weversonmp <weversonmnzs@gmail.com>
2023-07-30 12:48:35 -0300

    db update - new routes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
 import { openDb } from './configDB.js';
+import { createTable, insertPessoa, updatePessoa } from './controller/pessoa.js';
 import express from 'express';
-
 const app = express();
-app.use(express.json());
 
-openDb();
+app.use(express.json());
+createTable();
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
 });
 
 app.post('/pessoa', (req, res) => {
-  console.log(req.body)
+  insertPessoa(req.body);
   res.json({
-    "statusCode": 200
-  })
+    statusCode: 200,
+  });
 });
 
-app.listen(3000, () => console.log('Api rodando em http://localhost:3000'))
+app.put('/pessoa', (req, res) => {
+  if (req.body && !req.body.id) {
+    res.json({
+      statusCode: 400,
+      msg: 'Você precisa informar um id',
+    });
+  } else {
+    updatePessoa(req.body);
+    res.json({
+      statusCode: 200,
+    });
+  }
+});
+
+app.listen(3000, () => console.log('Api rodando em http://localhost:3000'));
